772f5089ed5d90b6fd7c442eb08fdb774123c616
DjuffinUA <70951333+DjuffinUA@users.noreply.github.com>
2026-02-13 21:43:12 +0200

    fix: correct edit rollback logic and clean duplicate warning logs

--- PWInstanceLauncher/Models/CharacterProfile.cs
+++ PWInstanceLauncher/Models/CharacterProfile.cs
@@ -1,15 +1,53 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text.Json.Serialization;
 
 namespace PWInstanceLauncher.Models
 {
-    public class CharacterProfile
+    public class CharacterProfile : INotifyPropertyChanged
     {
-        public string Name { get; set; } = "";
-        public string Login { get; set; } = "";
-        public string EncryptedPassword { get; set; } = "";
+        private string _name = string.Empty;
+        private string _login = string.Empty;
+        private string _encryptedPassword = string.Empty;
+        private string _runtimeStatus = "Offline";
+
+        public string Name
+        {
+            get => _name;
+            set => SetField(ref _name, value);
+        }
+
+        public string Login
+        {
+            get => _login;
+            set => SetField(ref _login, value);
+        }
+
+        public string EncryptedPassword
+        {
+            get => _encryptedPassword;
+            set => SetField(ref _encryptedPassword, value);
+        }
+
+        [JsonIgnore]
+        public string RuntimeStatus
+        {
+            get => _runtimeStatus;
+            set => SetField(ref _runtimeStatus, value);
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private bool SetField<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value))
+            {
+                return false;
+            }
+
+            field = value;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            return true;
+        }
     }
 }
