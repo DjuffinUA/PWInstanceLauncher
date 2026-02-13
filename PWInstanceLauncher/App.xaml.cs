243363bc41d6b7096fdf7cda047abd85dd01d37b
DjuffinUA <kapatsyn.ivan@gmail.com>
2026-02-13 14:36:24 +0200

    Добавьте файлы проекта.

---
+++ PWInstanceLauncher/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace PWInstanceLauncher
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
